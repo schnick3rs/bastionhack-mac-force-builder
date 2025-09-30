e812a1ceecc707a1ff311ab49e219a5ab903ed74
Moritz Halke <moritz.orth@googlemail.com>
2025-10-01 01:58:51 +0200

    seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,4 +19,28 @@ export default defineNuxtConfig({
   pinia: {
     storesDirs: ['~/stores/**'],
   },
+
+    app: {
+        head: {
+            title: 'MAC Attack Force Builder', // default fallback title
+            meta: [
+                { name: 'description', content: 'Build and manage your MAC force lists' },
+
+                { name: 'twitter:card', content: 'summary_large_image' },
+                { name: 'twitter:title', content: 'MAC Attack Force Builder' },
+                { name: 'twitter:description', content: 'Build and manage your MAC force lists' },
+                { name: 'twitter:image', content: 'https://mac-forces-0068b59196af.herokuapp.com/social.webp' },
+
+                { name: 'og:site_name', content: 'MAC Attack Force Builder' },
+                { name: 'og:type', content: 'website' },
+                { name: 'og:title', content: 'MAC Attack Force Builder' },
+                { name: 'og:description', content: 'Build and manage your MAC force lists' },
+                { name: 'og:image', content: 'https://mac-forces-0068b59196af.herokuapp.com/social.webp' },
+
+            ],
+            htmlAttrs: {
+                lang: 'en',
+            },
+        },
+    },
 })
