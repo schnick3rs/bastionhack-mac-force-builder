689eb4945654088e4af5fcabd7ae2b0d61a5b7ef
Moritz Halke <moritz.halke@ella-lab.io>
2025-10-06 14:33:39 +0200

    rework print view

--- app/pages/builder/[forceId]/view.vue
+++ app/pages/builder/[forceId]/view.vue
@@ -68,46 +68,50 @@ const usedHardware = computed((): HardwareModule[] => {
 
 <template>
 
+
+  <div class="flex justify-center mb-4">
+    <UFieldGroup size="xl">
+      <UButton color="neutral" :variant="suit === 'clubs' ? 'solid' : 'outline'" icon="i-game-icons-clubs" @click="suit = 'clubs'" />
+      <UButton color="neutral" :variant="suit === 'spades' ? 'solid' : 'outline'" icon="i-game-icons-spades" @click="suit = 'spades'" />
+      <UButton color="error" :variant="suit === 'hearts' ? 'solid' : 'outline'" icon="i-game-icons-hearts" @click="suit = 'hearts'" />
+      <UButton color="error" :variant="suit === 'diamonds' ? 'solid' : 'outline'" icon="i-game-icons-diamonds" @click="suit = 'diamonds'" />
+    </UFieldGroup>
+  </div>
+
   <div class="flex flex-col justify-center align-center" v-if="force">
 
-    <h1>{{ force.name }}</h1>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-8 pt-4" >
 
-    <div class="flex justify-center mb-4">
-      <UFieldGroup size="xl">
-        <UButton color="neutral" :variant="suit === 'clubs' ? 'solid' : 'outline'" icon="i-game-icons-clubs" @click="suit = 'clubs'" />
-        <UButton color="neutral" :variant="suit === 'spades' ? 'solid' : 'outline'" icon="i-game-icons-spades" @click="suit = 'spades'" />
-        <UButton color="error" :variant="suit === 'hearts' ? 'solid' : 'outline'" icon="i-game-icons-hearts" @click="suit = 'hearts'" />
-        <UButton color="error" :variant="suit === 'diamonds' ? 'solid' : 'outline'" icon="i-game-icons-diamonds" @click="suit = 'diamonds'" />
-      </UFieldGroup>
-    </div>
+      <!-- ENTRIES -->
+      <UPageList class="col-span-2" data="page-list">
 
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-8 pt-4" >
+        <UPageCard
+            v-for="(entry, index) in force.entries"
+            orientation="vertical"
+            class="mb-3"
+            :ui="{ container: 'sm:p-2' }"
+        >
+          <div class="flex flex-row gap-2">
 
-      <div>
-        <UPageList>
+            <!-- SUIT CARD -->
+            <UIcon :name="`i-game-icons-card-${ indexToCard[index] }-${ suit }`" class="size-16 shrink-0" :class="{ 'bg-error': ['hearts', 'diamonds'].includes(suit) }"/>
 
-          <UPageCard
-              v-for="(entry, index) in force.entries"
-              orientation="horizontal"
-              class="mb-3"
-              :ui="{ container: 'sm:p-2' }"
-          >
-            <div class="flex flex-row gap-2">
+            <!-- ENTRY CARD -->
+            <div class="flex-1 w-full">
 
-              <UIcon :name="`i-game-icons-card-${ indexToCard[index] }-${ suit }`" class="size-16 shrink-0" :class="{ 'bg-error': ['hearts', 'diamonds'].includes(suit) }"/>
+              <h2 class="font-bold text-xl">{{ entry.name || `${entry.size}x ${entry.unit.name}` }}</h2>
 
-              <div class="flex-1 w-full">
-
-                <h2 class="font-bold text-xl">{{ entry.name || `${entry.size}x ${entry.unit.name}` }}</h2>
-
-                <div class="mb-2">
-                  {{ displayClassificaition(entry) }}
-                  <span v-if="entry.classification === 'MAC'" class="flex-row">
+              <div class="mb-2">
+                {{ displayClassificaition(entry) }}
+                <span v-if="entry.classification === 'MAC'" class="flex-row">
                     <UIcon v-for="i in entry.class" name="i-material-symbols-light-check-box-outline-blank" class=""></UIcon>
                   </span>
-                </div>
+              </div>
 
-                <template v-if="entry.classification === 'MAC'">
+              <template v-if="entry.classification === 'MAC'">
+
+                <div class="grid grid-cols-2 gap-2">
+
                   <ul>
                     <li v-for="module in entry.modules" class="w-full flex flex-row justify-between items-center">
                       {{module.slot}} -
@@ -122,54 +126,63 @@ const usedHardware = computed((): HardwareModule[] => {
                       </div>
                     </li>
                   </ul>
-                  <div class="flex flex-row gap-1 mt-2">
-                    <h4 class="font-bold ">Notes</h4>
-                    <div class="w-full border-b-1 ml-2" style="height: 1px; border-color: rgba(0,0,0, 0.1); top: 12px; position: relative;"></div>
+
+                  <div>
+                    <div class="flex flex-row gap-1">
+                      <h4 class="font-bold ">Notes</h4>
+                      <div class="w-full border-b-1 ml-2" style="height: 1px; border-color: rgba(0,0,0, 0.1); top: 12px; position: relative;"></div>
+                    </div>
+                    <UTextarea class="w-full" :rows="5" placeholder="Perks, Flaws, Pilot Skills, ...."></UTextarea>
                   </div>
-                  <div class="h-24 border-gray-100 border-2"></div>
-                </template>
+
+                </div>
 
 
-                <template v-if="entry.classification === 'Formation'">
+              </template>
 
-                  <div class="flex flex-row gap-1 mt-2">
-                    <h4 class="font-bold ">Modules</h4>
-                    <div class="w-full border-b-1 ml-2" style="height: 1px; border-color: rgba(0,0,0, 0.1); top: 12px; position: relative;"></div>
-                  </div>
-                  <ul class="flex flex-wrap gap-2">
-                    <template v-for="(weapon) in entry.unit.weapons">
-                      <li>{{ buildWeaponDisplayString(weapon) }}</li>
-                      <span> ⸱ </span>
-                    </template>
 
-                    <template v-for="(hardware, index) in convertToNiceware(entry.unit.hardware)">
-                      <li>
-                        <span v-if="hardware.count > 1">2x </span>
-                        <span style="text-decoration: underline dashed; text-underline-offset: 4px">{{ hardware.name }}</span>
-                      </li>
-                      <span v-if="index < convertToNiceware(entry.unit.hardware).length -1"> ⸱ </span>
-                    </template>
-                  </ul>
+              <template v-if="entry.classification === 'Formation'">
 
-                </template>
+                <div class="flex flex-row gap-1 mt-2">
+                  <h4 class="font-bold ">Modules</h4>
+                  <div class="w-full border-b-1 ml-2" style="height: 1px; border-color: rgba(0,0,0, 0.1); top: 12px; position: relative;"></div>
+                </div>
+                <ul class="flex flex-wrap gap-2">
+                  <template v-for="(weapon) in entry.unit.weapons">
+                    <li>{{ buildWeaponDisplayString(weapon) }}</li>
+                    <span> ⸱ </span>
+                  </template>
 
-              </div>
+                  <template v-for="(hardware, index) in convertToNiceware(entry.unit.hardware)">
+                    <li>
+                      <span v-if="hardware.count > 1">2x </span>
+                      <span style="text-decoration: underline dashed; text-underline-offset: 4px">{{ hardware.name }}</span>
+                    </li>
+                    <span v-if="index < convertToNiceware(entry.unit.hardware).length -1"> ⸱ </span>
+                  </template>
+                </ul>
+
+              </template>
 
             </div>
 
-          </UPageCard>
+          </div>
 
-        </UPageList>
-      </div>
+        </UPageCard>
 
+      </UPageList>
+
+      <!-- RULES -->
       <div>
 
+        <h1 class="font-bold text-2xl">{{ force.name }}</h1>
 
-
-        <h1 class="font-bold text-3xl">{{ force.name }}</h1>
+        <div>
+          {{calculateForceCost(force)}}pt ⸱ {{force.entries.length}} entries
+        </div>
 
         <div v-if="faction">
-          <h2 class="font-bold text-2xl">A <em>{{ faction.name }}</em> Force</h2>
+          <h2 class="font-bold text-xl ">A <em>{{ faction.name }}</em> Force</h2>
 
           <div class="mt-4">
             <h4 class="font-bold text-xl pb-2 underline underline-offset-4 under">Faction Special Rule</h4>
@@ -179,6 +192,7 @@ const usedHardware = computed((): HardwareModule[] => {
             </p>
           </div>
         </div>
+
 
         <div class="mt-4">
           <h4 class="font-bold text-xl pb-2 underline underline-offset-4 under">Hardware Rules</h4>
