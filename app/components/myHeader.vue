78f2fbbe072b461964b4ce9ac65371127d83c72a
Moritz Halke <moritz.halke@ella-lab.io>
2025-10-08 14:28:17 +0200

    smaller home icon

--- app/components/myHeader.vue
+++ app/components/myHeader.vue
@@ -21,7 +21,7 @@ const items = computed<NavigationMenuItem[]>(() => [
 <template>
   <UHeader class="print:hidden">
     <template #title>
-      <NuxtImg src="mactory-square-800x800.jpg" class="w-8"></NuxtImg>
+      <NuxtImg src="android-chrome-192x192.png" class="w-8"></NuxtImg>
       MACtory
     </template>
     <UNavigationMenu :items="items" />
