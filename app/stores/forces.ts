7a5621b28b7830d96a945f0ecfc24fa7b806825a
Moritz Halke <moritz.orth@googlemail.com>
2026-02-03 21:00:37 +0100

    cleanup

--- app/stores/forces.ts
+++ app/stores/forces.ts
@@ -1,5 +1,5 @@
 import {defineStore} from "pinia";
-import type {Auxiliary, Entry, Force, Formation, MAC, RemoteAsset, WeaponProfile,} from "~~/types/unit";
+import type {Auxiliary, Entry, Force, Formation, MAC, RemoteAsset } from "~~/types/unit";
 import {sortForceEntries} from "#shared/utils/forces";
 
 import { uniqueUsernameGenerator as gen, nouns } from 'unique-username-generator';
